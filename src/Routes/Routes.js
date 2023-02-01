1e963588a26aae590293b65d382d9d08b95516d2
Deepak Singh <singhdeepak5219@gmail.com>
2023-02-01 22:04:13 +0530

    initial fixes

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom';
 import {Route,Routes} from 'react-router-dom'
 import {Login,Signup,SuccessPage} from '../components/Auth';
+import {Dashboard} from '../pages';
 
 
 const AppRoutes = () => {
@@ -11,6 +12,8 @@ const AppRoutes = () => {
             <Route path='/' element={<Login/>}/>
             <Route path='/signup' element={<Signup/>}/>
             <Route path='/success' element={<SuccessPage/>}/>
+            <Route path=':token/dashboard' element={<Dashboard/>}/>
+
         </Routes>
     </Router>
   )
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>User Dashboard</div>
+  )
+}
+
+export default Dashboard;
